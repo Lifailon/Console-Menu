4da10599750f17eeb52a0553d00dbda0654bfc9f
Alex Kup <116945542+Lifailon@users.noreply.github.com>
2023-10-12 20:22:51 +0300

    ver 0.1

---
+++ Console-Menu.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# Source: https://github.com/Lifailon/Console-Menu
+# ©2023 Lifailon
+# Apache License 2.0
+menu=(
+"Process Stats"
+"Network Stats"
+"Disk Stats"
+"Exit"
+)
+
+# ASCII Art generated: https://patorjk.com/software/taag/#p=display&f=Ivrit&t=Console%20-%20Menu
+logo=(
+"   ____                      _                __  __                   "
+"  / ___|___  _ __  ___  ___ | | ___          |  \/  | ___ _ __  _   _  "
+" | |   / _ \| '_ \/ __|/ _ \| |/ _ \  _____  | |\/| |/ _ \ '_ \| | | | "
+" | |__| (_) | | | \__ \ (_) | |  __/ |_____| | |  | |  __/ | | | |_| | "
+"  \____\___/|_| |_|___/\___/|_|\___|         |_|  |_|\___|_| |_|\__,_| "
+"                                                                       "
+)
+selected=0
+
+function show-menu {
+    clear
+    rows=$(tput lines)
+    cols=$(tput cols)
+    menu_length=${#menu[@]}
+    current_row=$((rows / 2 - menu_length / 2))
+
+    tput cup $current_row $((cols / 2 - ${#menu[$i]} / 2))
+    echo -e $logo
+
+    # logo
+    for i in "${!logo[@]}"; do
+        tput cup $current_row $((cols / 2 - ${#logo[$i]} / 2))
+        echo "${logo[$i]}"
+        ((current_row++))
+    done
+
+    # Menu
+    for i in "${!menu[@]}"; do
+        if [[ $i -eq $selected ]]; then
+            tput cup $current_row $((cols / 2 - ${#menu[$i]} / 2))
+            echo "> ${menu[$i]}"
+        else
+            tput cup $current_row $((cols / 2 - ${#menu[$i]} / 2))
+            echo "  ${menu[$i]}"
+        fi
+        ((current_row++))
+    done
+}
+
+while true; do
+    show-menu
+    read -n1 key
+    case $key in
+        "")
+            clear
+            if [[ $selected -eq $(( ${#menu[@]} - 1 )) ]]; then # Exit
+                break
+            else
+                line=${menu[$selected]}
+                # Main Code
+                if [ "$line" == "Process Stats" ]
+                    then
+                    ps=$(ps -Ao comm,user,cputime,pcpu,pmem,sz,rss,vsz,nlwp,psr,pri,ni)
+                    printf "%s\n" "${ps[@]}" | head -n 1
+                    printf "%s\n" "${ps[@]}" | sort -r -nk4 | head -n 20
+                elif [ "$line" == "Network Stats" ]
+                    then
+                    ss -t
+                elif [ "$line" == "Disk Stats" ]
+                    then
+                    lsblk -e7
+                fi
+                read -p "Press Enter to continue..."
+            fi
+        ;;
+        # up
+        "A"|"a")
+            if [[ $selected -gt 0 ]]; then
+                selected=$((selected-1))
+            fi
+        ;;
+        "B"|"b")
+         # down
+            if [[ $selected -lt $(( ${#menu[@]} - 1 )) ]]; then
+                selected=$((selected+1))
+            fi
+        ;;
+    esac
+done
